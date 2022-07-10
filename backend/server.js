54e7dca37ce66df1ae0ab397d4ff2887e17d6a5c
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-07-10 22:55:48 +0300

    Connecting To The Databse & Coloring The Console

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dontenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dontenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,5 +28,5 @@ const environment = process.env.NODE_ENV || "development";
 
 app.listen(
   PORT,
-  console.log(`Server running in ${environment} mode on port ${PORT}`)
+  console.log(`Server running in ${environment} mode on port ${PORT}`.yellow.bold)
 );
