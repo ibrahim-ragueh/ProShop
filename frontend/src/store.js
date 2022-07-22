f9423b9a89575d0fb3c3bafb703269cdb5b27c0e
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-07-22 22:09:18 +0300

    Product Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const reducers = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
