d065e62ecbfebe747b839a4135644d470e58c61a
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-08-08 00:44:29 +0300

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,41 @@ const authUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(401);
-    throw new Error("Invalid email or password");
+    throw new Error("Invalid email or password!");
+  }
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data!");
   }
 });
 
@@ -42,8 +76,8 @@ const getUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User not found");
+    throw new Error("User not found!");
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,9 +30,23 @@ const userSchema = mongoose.Schema(
 // A method to compare the entered plain password with the one saved in the database which is encrypted
 // This method can be accessed through stantiated user
 // When we call matchPassword method on specific user we get the encrypted password for that user through this.password
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Adding a middleware to encrypt the password before saved
+userSchema.pre("save", async function (next) {
+  // This if statement is to check if the password field is sent or modified
+  // isModified is a mongoose method to check if somthing is modified and in this case the password field
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // create a salt to hash the password asynchronously with 10 number of rounds
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
