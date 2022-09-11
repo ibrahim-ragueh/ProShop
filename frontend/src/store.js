ec77c52940e5c9d272bc7b20d30c1fdbe74e9e50
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-09-11 22:25:21 +0300

    Show Orders On Profile + Fixing Typo In Order Model

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderListMyReducer,
   orderPayReducer,
 } from "./reducers/orderReducers";
 
@@ -28,6 +29,7 @@ const reducers = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
