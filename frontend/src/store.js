f71fb3c79c8b7edbfe244a01746b6cc5e25f2f84
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-09-22 17:43:27 +0300

    Update User Functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
   userUpdateProfileReducer,
   userListReducer,
   userDeleteReducer,
+  userUpdateReducer,
 } from "./reducers/userReducers";
 import {
   orderCreateReducer,
@@ -30,6 +31,7 @@ const reducers = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
