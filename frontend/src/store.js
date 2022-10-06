9abc60601b14be296c4c6e6b1b78807978393af8
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2022-10-06 22:53:19 +0300

    Mark Order As Delivered & BUG FIX For Warning In bootstrap.min.css

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,6 +19,7 @@ import {
 } from "./reducers/userReducers";
 import {
   orderCreateReducer,
+  orderDeliverReducer,
   orderDetailsReducer,
   orderListMyReducer,
   orderListReducer,
@@ -42,6 +43,7 @@ const reducers = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
   orderListMy: orderListMyReducer,
   orderList: orderListReducer,
 });
