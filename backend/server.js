b676838d07ae0d0704a522ff1fa7edb5a50be2f3
Ibrahim Ragueh <ibrahim.raji2@gmail.com>
2023-01-29 21:32:36 +0300

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,19 @@ const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-};
+}
 
 // A middleware in order to parse the req.body in userController.js & it is called body parser
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
 
 app.use("/api/products/", productRoutes);
 app.use("/api/users/", userRoutes);
 app.use("/api/orders/", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
 // uploads folder is not accessable by default -->
 // --> and needed to be made static folder so that it can get loaded in the browser
@@ -39,9 +39,18 @@ app.use("/api/upload", uploadRoutes);
 const __dirname = path.resolve(); // To mimic (__dirname) as it is only available in common js & not in ES syntax
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //get any route that isn't our api to point to that index.html file is in the build folder
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
+  });
+}
 
 app.use(notFound);
 
